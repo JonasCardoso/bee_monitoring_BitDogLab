b8115d7ad1eb3e9a80186454f08d8c1864b51073
Jonas Cardoso Gonçalves <j256453@dac.unicamp.br>
2024-06-08 12:00:00 -0300

    main: adicionar configuração do UTC

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import network
 import ufirebase as firebase
 import openweathermap as weather
 import time
+import utime
+
+# Define the offset for 'America/Sao_Paulo' in seconds
+# São Paulo is UTC-3
+timezone_offset = -3 * 3600
+current_time = utime.time()
+adjusted_time = current_time + timezone_offset
+utime.localtime(adjusted_time)
+local_time = utime.localtime()
 
 GLOB_WLAN = network.WLAN(network.STA_IF)
 GLOB_WLAN.active(True)
@@ -24,5 +33,6 @@ while 1:
     timestamp = f"{dia}_{mes}_{ano}-{hora}_{minuto}_{segundo}"
     print("Timestamp:", timestamp)
     weather_data = weather.get_weather_data()
-    dados = {"temperatura": 1, "umidade": 2, "som": 2, "pressao": 2, "openweathermap": weather_data}
+    sensor_data = {"temperatura": 1, "umidade": 2, "som": 2, "pressao": 2}
+    dados = {"sensor": sensor_data, "openweathermap": weather_data}
     firebase.put("bee_data/" + timestamp, dados, bg=0)
